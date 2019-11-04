b46a6b30d2a55d276e378bb18a7073aa0262e977
Mario Carneiro <di.gama@gmail.com>
2019-11-04 16:35:45 -0500

    bugfix

--- test/ring.lean
+++ test/ring.lean
@@ -17,3 +17,6 @@ example {α} [linear_ordered_field α] (a b c : α) :
   b ^ 2 - 4 * c * a = -(4 * c * a) + b ^ 2 := by ring
 example {α} [linear_ordered_field α] (a b c: α) :
   b ^ 2 - 4 * a * c = 4 * a * 0 + b * b - 4 * a * c := by ring
+example {α} [comm_semiring α] (x y z : α) (n : ℕ) :
+  (x + y) * (z * (y * y) + (x * x ^ n + (1 + ↑n) * x ^ n * y)) =
+    x * (x * x ^ n) + ((2 + ↑n) * (x * x ^ n) * y + (x * z + (z * y + (1 + ↑n) * x ^ n)) * (y * y)) := by ring
